aca6b94b2ebdde103079307100ae13e488270323
mrlmhl <meralorguc1@gmail.com>
2023-12-15 12:46:25 +0300

    first commit

---
+++ src/components/products/ProductCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// const ProductCard = ({product}) => {
+//     const { id, title, image, price }= product
+//   return (
+//     <div key={id}>
+//           <div>
+//             <h3>{price}</h3>
+//           </div>
+//           <img src={image} alt={title} width="100px" />
+//           <div>
+//             <h2>{title}</h2>
+//           </div>
+//         </div>
+//   );
+// };
+
+// export default ProductCard
+
+
+const ProductCard = ({ title, image, price }) => {
+    //   console.log(props);
+    return (
+      <div className="card">
+        <div className="price">
+          <h3>{price} $</h3>
+        </div>
+        <img src={image} alt={title}/>
+        <div className="card__over">
+          <h2>{title}</h2>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ProductCard;
